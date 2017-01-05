5e3236b09f28abf415fef03de5597948b9484c22
Unknown <sonya.grosjean0195@gmail.com>
2017-01-05 13:40:56 +0100

    Modification img

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// var dr = document.getElementById("droite");
+// dr.style.visibility = "hidden";
+
+// function mouvleft(){
+	// var box = document.getElementById("tourne");
+	// var ga = document.getElementById("gauche");
+	// var dr = document.getElementById("droite");
+
+	// if(box.style.left == 0+"px"){
+		// box.style.left = -400+"px";
+		// dr.style.visibility = "visible";
+		// ga.style.visibility = "visible";
+	// }
+	
+	// else if(box.style.left == -400+"px"){
+		// box.style.left = -800+"px";
+		// ga.style.visibility = "hidden";
+	// }
+	
+	// else{
+		// box.style.left = 0+"px";
+	// }
+// }
+
+// function mouvright(){
+	// var box = document.getElementById("tourne");
+	// var ga = document.getElementById("gauche");
+	// var dr = document.getElementById("droite");
+	
+	// if(box.style.left == -800+"px"){
+		// box.style.left = -400+"px";
+		// dr.style.visibility = "visible";
+		// ga.style.visibility = "visible";
+	// }
+	
+	// else if(box.style.left == -400+"px"){
+		// box.style.left = -0+"px";
+		// dr.style.visibility = "hidden";
+	// }
+	
+	// else{
+		// box.style.left = -800+"px";
+	// }
+// }
+
+$(document).ready(function(){
+	$("img").click(function(){
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
